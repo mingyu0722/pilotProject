017e94b0e519c01a07a77ef015040f218713cc65
Hyowon Park <sayyoursong@gmail.com>
2019-11-03 22:34:57 +0900

    노트북 환경에 맞게 세팅 변경

--- src/test/java/MySQLConnectionTest.java
+++ src/test/java/MySQLConnectionTest.java
@@ -7,9 +7,9 @@ import java.sql.DriverManager;
 public class MySQLConnectionTest {
 
     private static final String DRIVER      = "com.mysql.jdbc.Driver";
-    private static final String URL         = "jdbc:mysql://127.0.0.1:3306/board?useSSL=false&serverTimezone=Asia/Seoul";
+    private static final String URL         = "jdbc:mysql://127.0.0.1:3307/pilot_project?useSSL=false&serverTimezone=Asia/Seoul";
     private static final String USER        = "root";
-    private static final String PASSWORD    = "@clsrn7027";
+    private static final String PASSWORD    = "1234";
 
     @Test
     public void testConnection() throws Exception {
